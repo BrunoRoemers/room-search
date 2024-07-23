7c4925cc6e4bfac50c737c4f706e77351e056790
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-07-23 21:51:48 +0200

    add hello world to google sheet

--- src/app/api/v1/submit/route.ts
+++ src/app/api/v1/submit/route.ts
@@ -1,5 +1,23 @@
+import { google } from "googleapis";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  return NextResponse.json({ message: "Hello from the API!" });
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+  const sheets = google.sheets({ version: "v4", auth });
+
+  const response = await sheets.spreadsheets.values.append({
+    spreadsheetId: process.env.SHEET_ID,
+    range: process.env.SHEET_TAB,
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      values: [["hello world", "123"]],
+    },
+  });
+
+  return NextResponse.json(
+    { message: response.statusText },
+    { status: response.status }
+  );
 }
