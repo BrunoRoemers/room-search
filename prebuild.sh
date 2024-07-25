124f83a254b6d9b49435bb589aab72b1046fa6be
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-07-25 23:39:01 +0200

    use rollup in prebuild script

--- prebuild.sh
+++ prebuild.sh
@@ -3,5 +3,5 @@
 # make secrets.json available to nextjs backend
 echo "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" | base64 --decode > secrets.json
 
-# compile bookmarklet.ts to downloadable bookmarklet.js
-tsc src/bookmarklet.ts --outFile public/bookmarklet.js
+# compile src/bookmarklet to downloadable bookmarklet.js
+npx rollup --config
