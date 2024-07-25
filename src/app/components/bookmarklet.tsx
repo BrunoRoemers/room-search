e3549084f37a64530b9e8392615ca370a66de1e7
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-07-25 20:13:45 +0200

    avoid react warning about javascript links

---
+++ src/app/components/bookmarklet.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  label: string;
+  code: string;
+}
+
+const Bookmarklet = ({ label, code }: Props) => {
+  const html = `<a
+      className="underline hover:decoration-wavy text-xl"
+      href="${encodeURI(code)}"
+    >${label}</a>`;
+
+  return <span dangerouslySetInnerHTML={{ __html: html }}></span>;
+};
+
+export default Bookmarklet;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-const generateBookmarklet = (secret: string, rating: number) =>
-  encodeURI(
-    `javascript:(function(){
-      window.ROOM_SEARCH_DATA = {
-        secret: '${secret}',
-        rating: ${rating},
-      };
-      const script = document.createElement("script");
-      script.src="${process.env.NEXT_PUBLIC_BASE_URL}/bookmarklet.js";
-      document.body.appendChild(script);
-    })()`
-  );
+import Bookmarklet from "./components/bookmarklet";
+
+const generateBookmarkletCode = (secret: string, rating: number) =>
+  `javascript:(function(){
+    window.ROOM_SEARCH_DATA = {
+      secret: '${secret}',
+      rating: ${rating},
+    };
+    const script = document.createElement("script");
+    script.src="${process.env.NEXT_PUBLIC_BASE_URL}/bookmarklet.js";
+    document.body.appendChild(script);
+  })()`;
 
 const Home = () => {
   return (
@@ -19,12 +19,10 @@ const Home = () => {
         <ul className="flex gap-x-10 justify-center">
           {[...Array(3)].map((_, i) => (
             <li key={i}>
-              <a
-                className="underline hover:decoration-wavy text-xl"
-                href={generateBookmarklet("TODO", i + 1)}
-              >
-                {"☆".repeat(i + 1)}
-              </a>
+              <Bookmarklet
+                label={"☆".repeat(i + 1)}
+                code={generateBookmarkletCode("TODO", i + 1)}
+              />
             </li>
           ))}
         </ul>
