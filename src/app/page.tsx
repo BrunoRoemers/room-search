c17836fb1d42545a8002d51fea86819e4cd6a342
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-07-27 13:46:07 +0200

    inject secret into bookmarklet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,61 @@
+"use client";
+
+import { useState, type KeyboardEvent } from "react";
 import Bookmarklet from "./components/bookmarklet";
 import generateBookmarkletCode from "./utils/generateBookmarkletCode";
 
 const Home = () => {
+  const [secret, setSecret] = useState<string | null>(null);
+
+  const handleKeyUp = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      setSecret(event.currentTarget.value);
+    }
+  };
+
   return (
     <div className="h-full flex flex-col bg-black text-lime-400 text-center font-mono">
       <div className="grow flex justify-center items-center">
         <div className="space-y-14">
           <h1 className="text-3xl">Room Search</h1>
-          <ul className="flex gap-x-10 justify-center">
-            {[...Array(3)].map((_, i) => (
-              <li key={i}>
-                <Bookmarklet
-                  label={"☆".repeat(i + 1)}
-                  code={generateBookmarkletCode("TODO", i + 1)}
-                />
-              </li>
-            ))}
-          </ul>
-          <ol className="space-y-2">
-            <li>Bookmark each of the links above.</li>
-            <li>Search for an apartment.</li>
-            <li>Click one of the bookmarks.</li>
-            <li>
-              Check out this{" "}
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={`https://docs.google.com/spreadsheets/d/${process.env.NEXT_PUBLIC_SHEET_ID}/edit?gid=${process.env.NEXT_PUBLIC_SHEET_GID}`}
-                className="underline hover:decoration-wavy"
-              >
-                Google Sheet
-              </a>
-              .
-            </li>
-          </ol>
+          {secret === null ? (
+            <input
+              type="password"
+              className="bg-black text-center"
+              placeholder="enter secret"
+              onKeyUp={handleKeyUp}
+            />
+          ) : (
+            <>
+              <ul className="flex gap-x-10 justify-center">
+                {[...Array(3)].map((_, i) => (
+                  <li key={i}>
+                    <Bookmarklet
+                      label={"☆".repeat(i + 1)}
+                      code={generateBookmarkletCode(secret, i + 1)}
+                    />
+                  </li>
+                ))}
+              </ul>
+              <ol className="space-y-2">
+                <li>Bookmark each of the links above.</li>
+                <li>Search for an apartment.</li>
+                <li>Click one of the bookmarks.</li>
+                <li>
+                  Check out this{" "}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={`https://docs.google.com/spreadsheets/d/${process.env.NEXT_PUBLIC_SHEET_ID}/edit?gid=${process.env.NEXT_PUBLIC_SHEET_GID}`}
+                    className="underline hover:decoration-wavy"
+                  >
+                    Google Sheet
+                  </a>
+                  .
+                </li>
+              </ol>
+            </>
+          )}
         </div>
       </div>
       <div className="text-lime-700 text-xs pb-4">
