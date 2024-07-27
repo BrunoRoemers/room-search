d6f1d75bf0b249ffd98a725e7248e5ee0bbc97a7
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-07-27 16:49:05 +0200

    add users page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={`${inter.className} h-full`}>{children}</body>
+      <body className={`${inter.className} h-full`}>
+        <div className="h-full flex flex-col bg-black text-lime-400 text-center font-mono">
+          <div className="grow flex justify-center items-center">
+            {children}
+          </div>
+          <div className="text-lime-700 text-xs pb-4">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/BrunoRoemers/room-search"
+            >
+              source code
+            </a>
+            <span> · </span>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://app.netlify.com/sites/room-search/deploys"
+            >
+              deploy status
+            </a>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
