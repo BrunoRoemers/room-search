d12d1c350ffc1b9fcfeeb070dd8b100268415674
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-07-27 12:38:34 +0200

    add link to source code and deploy status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,36 +3,55 @@ import generateBookmarkletCode from "./utils/generateBookmarkletCode";
 
 const Home = () => {
   return (
-    <div className="h-full flex justify-center items-center bg-black text-lime-400 text-center font-mono pb-20">
-      <div className="space-y-14">
-        <h1 className="text-3xl">Room Search</h1>
-        <ul className="flex gap-x-10 justify-center">
-          {[...Array(3)].map((_, i) => (
-            <li key={i}>
-              <Bookmarklet
-                label={"☆".repeat(i + 1)}
-                code={generateBookmarkletCode("TODO", i + 1)}
-              />
+    <div className="h-full flex flex-col bg-black text-lime-400 text-center font-mono">
+      <div className="grow flex justify-center items-center">
+        <div className="space-y-14">
+          <h1 className="text-3xl">Room Search</h1>
+          <ul className="flex gap-x-10 justify-center">
+            {[...Array(3)].map((_, i) => (
+              <li key={i}>
+                <Bookmarklet
+                  label={"☆".repeat(i + 1)}
+                  code={generateBookmarkletCode("TODO", i + 1)}
+                />
+              </li>
+            ))}
+          </ul>
+          <ol className="space-y-2">
+            <li>Bookmark each of the links above.</li>
+            <li>Search for an apartment.</li>
+            <li>Click one of the bookmarks.</li>
+            <li>
+              Check out this{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://docs.google.com/spreadsheets/d/${process.env.NEXT_PUBLIC_SHEET_ID}/edit?gid=${process.env.NEXT_PUBLIC_SHEET_GID}`}
+                className="underline hover:decoration-wavy"
+              >
+                Google Sheet
+              </a>
+              .
             </li>
-          ))}
-        </ul>
-        <ol className="space-y-2">
-          <li>Bookmark each of the links above.</li>
-          <li>Search for an apartment.</li>
-          <li>Click one of the bookmarks.</li>
-          <li>
-            Check out this{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={`https://docs.google.com/spreadsheets/d/${process.env.NEXT_PUBLIC_SHEET_ID}/edit?gid=${process.env.NEXT_PUBLIC_SHEET_GID}`}
-              className="underline hover:decoration-wavy"
-            >
-              Google Sheet
-            </a>
-            .
-          </li>
-        </ol>
+          </ol>
+        </div>
+      </div>
+      <div className="text-lime-700 text-xs pb-4">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/BrunoRoemers/room-search"
+        >
+          source code
+        </a>
+        <span> · </span>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://app.netlify.com/sites/room-search/deploys"
+        >
+          deploy status
+        </a>
       </div>
     </div>
   );
