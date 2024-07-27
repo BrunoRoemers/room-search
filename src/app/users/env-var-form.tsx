993083600f1926cfff48a18d472db59046e007f3
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-07-27 19:47:42 +0200

    detect duplicate secrets

--- src/app/users/env-var-form.tsx
+++ src/app/users/env-var-form.tsx
@@ -5,10 +5,11 @@ export interface Props {
   label: string;
   users: User[];
   setUsers: (users: User[]) => void;
+  hasError: boolean;
   setError: (error: any | null) => void;
 }
 
-const EnvVarForm = ({ label, users, setUsers, setError }: Props) => {
+const EnvVarForm = ({ label, users, setUsers, hasError, setError }: Props) => {
   const [value, setValue] = useState(serializeUsers(users));
 
   useEffect(() => setValue(serializeUsers(users)), [users]);
@@ -34,7 +35,7 @@ const EnvVarForm = ({ label, users, setUsers, setError }: Props) => {
         placeholder="<empty>"
         onChange={(event) => setValue(event.currentTarget.value)}
         onKeyUp={handleKeyUp}
-        className="text-ellipsis"
+        className={"text-ellipsis" + (hasError ? " text-rose-800" : "")}
       />
     </label>
   );
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { emptyUser, type User } from "@/models/user";
 import UserForm from "./user-form";
 import EnvVarForm from "./env-var-form";
@@ -8,6 +8,18 @@ import EnvVarForm from "./env-var-form";
 const UsersPage = () => {
   const [error, setError] = useState<any | null>(null);
   const [users, setUsers] = useState<User[]>([emptyUser]);
+
+  useEffect(() => {
+    const secrets = new Set();
+    for (const user of users) {
+      if (secrets.has(user.secret)) {
+        setError("All secrets must be unique");
+        return;
+      }
+      secrets.add(user.secret);
+    }
+    setError(null);
+  }, [users]);
 
   const replaceUser = (i: number, newUser: User | null) =>
     setUsers((prevUsers) =>
@@ -21,24 +33,28 @@ const UsersPage = () => {
   return (
     <div className="space-y-14">
       <h1 className="text-3xl">Configure Users</h1>
-      <div className="space-y-4">
-        {error !== null && (
-          <div className="text-rose-800">{error.toString()}</div>
-        )}
-        {users.map((user, i) => (
-          <UserForm
-            key={i}
-            user={user}
-            setUser={(newUser) => replaceUser(i, newUser)}
-          />
-        ))}
-      </div>
+      {error !== null && (
+        <div className="text-rose-800">{error.toString()}</div>
+      )}
+      {users.length > 0 && (
+        <ul className="space-y-4">
+          {users.map((user, i) => (
+            <li key={i}>
+              <UserForm
+                user={user}
+                setUser={(newUser) => replaceUser(i, newUser)}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
       <button onClick={appendUser}>Add User</button>
       <div>
         <EnvVarForm
           label="API_USERS_BASE64="
           users={users}
           setUsers={setUsers}
+          hasError={error !== null}
           setError={setError}
         />
       </div>
