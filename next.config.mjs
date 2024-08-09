0621c01a334304deca5880a16d9ed43811ccb007
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-08-09 22:43:26 +0200

    Attempt to pass secrets.json to nextjs functions

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,12 @@ const nextConfig = {
       },
     ];
   },
+  experimental: {
+    // docs: https://vercel.com/guides/how-can-i-use-files-in-serverless-functions
+    outputFileTracingIncludes: {
+      "/api/v1/submit": ["./secrets.json"],
+    },
+  },
 };
 
 export default nextConfig;
