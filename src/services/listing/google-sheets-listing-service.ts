ae2ab3025d33f76658876f40d55c33948e5233b3
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-08-12 19:49:31 +0200

    Move listing to sheet row conversion to separate method

--- src/services/listing/google-sheets-listing-service.ts
+++ src/services/listing/google-sheets-listing-service.ts
@@ -3,36 +3,46 @@ import type ListingService from "./listing-service";
 import { google } from "googleapis";
 
 export default class GoogleSheetsListingService implements ListingService {
+  // add a listing to the sheet
   async add(listing: Listing): Promise<Response> {
-    const sheets = await this.getSheets();
-    const response = await sheets.spreadsheets.values.append({
-      spreadsheetId: process.env.NEXT_PUBLIC_SHEET_ID,
-      range: process.env.SHEET_RANGE,
-      valueInputOption: "USER_ENTERED",
-      requestBody: {
-        values: [
-          [
-            listing.id,
-            listing.addedBy,
-            listing.url,
-            listing.rating,
-            listing.address,
-            listing.rent,
-            listing.costs,
-            listing.bedrooms.length,
-            listing.bathrooms.length,
-            listing.availabilityDate.toDateString(),
-          ],
-        ],
-      },
-    });
-    return response;
+    // TODO if exists, update the listing
+    return this.append(listing);
   }
 
+  // get the sheets API client
   private async getSheets(): Promise<any> {
     const auth = await google.auth.getClient({
       scopes: ["https://www.googleapis.com/auth/spreadsheets"],
     });
     return google.sheets({ version: "v4", auth });
+  }
+
+  // convert a listing to an array with one value for each column
+  private toRow(listing: Listing) {
+    return [
+      listing.id,
+      listing.addedBy,
+      listing.url,
+      listing.rating,
+      listing.address,
+      listing.rent,
+      listing.costs,
+      listing.bedrooms.length,
+      listing.bathrooms.length,
+      listing.availabilityDate.toDateString(),
+    ];
+  }
+
+  // add a row to the bottom of the sheet
+  private async append(listing: Listing): Promise<Response> {
+    const sheets = await this.getSheets();
+    return await sheets.spreadsheets.values.append({
+      spreadsheetId: process.env.NEXT_PUBLIC_SHEET_ID,
+      range: process.env.SHEET_RANGE,
+      valueInputOption: "USER_ENTERED",
+      requestBody: {
+        values: [this.toRow(listing)],
+      },
+    });
   }
 }
